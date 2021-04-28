fc6dce15a80df2498e35edfd4b02c64e75a8ce75
Jose Gomes <up201308141@fe.up.pt>
2021-04-28 10:23:32 +0100

    upadating video list

--- Project/media-app/src/components/MyVideos.js
+++ Project/media-app/src/components/MyVideos.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Card, Container, Button, Row, Col} from 'react-bootstrap'
 import MyNavbar from './MyNavbar'
 import '../styles/MyVideos.css'
@@ -10,18 +10,12 @@ import ReactPlayer from 'react-player'
 export default function MyVideos() {
 
     const { currentUser } = useAuth()
-//    const [list, setList] = useState ([])
+    const [list, setList] = useState ([])
     // Create a reference under which you want to list
     var listRef = storage.ref(`user/${currentUser.uid}`)
     const history = useHistory();
     const [url, setUrl] = useState('');
-    const [videoList,setVideoList] = useState([])
     var num = 1
-
-    function pushVideo(video) {
-        setVideoList(videoList.push(video))
-    } 
-
     function navigateTo (navurl){
         console.log(navurl);
         history.push({
@@ -31,23 +25,18 @@ export default function MyVideos() {
             console.log(navurl);
     } 
     
-
-    useEffect(()=> {
-        listRef.listAll()
+    // Find all the prefixes and items.
+    listRef.listAll()
         .then((res) => {
-            
-            
+            var list2 = []
             res.items.forEach((itemRef) => {
                 itemRef.getDownloadURL().then((url) => setUrl(url))
-                pushVideo(url)
+                list2.push(url)
             });
+            setList(list2)
         }).catch((error) => {
             // Uh-oh, an error occurred!
         });
-
-    },[]) 
-    // Find all the prefixes and items.
-
         
     return(
         <>
@@ -55,7 +44,7 @@ export default function MyVideos() {
             <Container fluid className='main-container justify-content-center text-center mt-2'>
             <Card className='file-video-card'>
                     <Card.Body>
-            {videoList.map((itemRef) => (
+            {list.map((itemRef) => (
                 <Row>
                     <Col>
                         <div>
